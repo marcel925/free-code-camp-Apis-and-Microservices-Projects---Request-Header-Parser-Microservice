3b40a4bd354e273255a4edf436b38840f38df0fe
Glitch (ordinary-clover) <none>
2018-12-29 02:51:33 +0000

    💰⭐️ Updated with Glitch

---
+++ server.js
@@ -0,0 +1,62 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+var axios = require('axios');
+
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+var ipaddress;
+var language;
+var software;
+var software2;
+
+axios.get('https://ipapi.co/json')
+  .then(response => {
+    ipaddress = response.data.ip;
+    language = response.data.languages;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+axios.get('https://www.whatsmyua.info/api/v1/ua')
+  .then(response => {
+    software = response.data[0].ua.rawUa;
+    software2 = response.data[1].device.description;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+app.get("/api/whoami", function (req, res) {
+  res.json({"ipaddress": ipaddress, "language": language, "software": software});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
